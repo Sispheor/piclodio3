025ea6d85873508ff8335c7750516f07e761c2e8
nico <nico.marcq@gmail.com>
2017-01-02 17:22:43 +0100

    delete the last mp3 at each new upload

--- back/webapi/views/BackupFileView.py
+++ back/webapi/views/BackupFileView.py
@@ -1,3 +1,5 @@
+import os
+
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -19,6 +21,11 @@ class BackupFileView(APIView):
         serializer = FileUploadSerializer(data=request.data)
 
         if serializer.is_valid():
+            # delete the existing MP3 file if exist
+            backup_files = BackupMusic.objects.all()
+            for el in backup_files:
+                os.remove(el.backup_file.path)
+                el.delete()
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
