4670a0c6ec0965ab980cbe731120e0a7d937267f
nico <nico.marcq@gmail.com>
2017-01-05 12:00:13 +0100

    move threaded player into the Player file

--- back/webapi/PlayerManager.py
+++ back/webapi/PlayerManager.py
@@ -1,5 +1,7 @@
 import os
 import subprocess
+import threading
+from time import sleep
 
 
 class PlayerManager(object):
@@ -9,7 +11,7 @@ class PlayerManager(object):
     MPLAYER_EXEC_PATH = "/usr/bin/mplayer"
 
     @classmethod
-    def play(cls, url):
+    def play(cls, url, blocking_thread=False):
         # kill process if already running
         if cls.is_started():
             cls.stop()
@@ -22,12 +24,15 @@ class PlayerManager(object):
         mplayer_command.extend([url])
         print("Mplayer cmd: %s" % str(mplayer_command))
 
-        fnull = open(os.devnull, 'w')
-        # subprocess.Popen(mplayer_command, stdout=fnull, stderr=fnull)
-        # blocking thread
-        p = subprocess.Popen(mplayer_command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        p.communicate()
-        print "Mplayer stopped"
+        if blocking_thread:
+            # blocking thread
+            p = subprocess.Popen(mplayer_command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            p.communicate()
+            # the following line will only be printed when the process mplayer will be killed
+            print "Mplayer stopped"
+        else:
+            fnull = open(os.devnull, 'w')
+            subprocess.Popen(mplayer_command, stdout=fnull, stderr=fnull)
 
     @classmethod
     def stop(cls):
@@ -46,3 +51,47 @@ class PlayerManager(object):
             return False
         else:
             return True
+
+
+class ThreadTimeout(object):
+    def __init__(self, callback_instance, backup_instance, timeout):
+        self.callback_instance = callback_instance
+        self.backup_instance = backup_instance
+        self.timeout = timeout
+        self.main_thread = None
+
+    def run(self):
+        def target():
+            print 'Thread started'
+            self.callback_instance.start()
+            print 'Thread finished'
+
+        def waiting_target():
+            print "Wait %s seconds before checking if the thread is alive" % self.timeout
+            sleep(self.timeout)
+            if self.main_thread.is_alive():
+                print 'Thread is alive'
+                # thread.join()
+            else:
+                print 'Thread is dead'
+                if self.backup_instance is not None:
+                    print "Playing backup file"
+                    self.backup_instance.start()
+                else:
+                    print "Not backup file provided"
+
+        # start a thread to play the webradio
+        self.main_thread = threading.Thread(target=target)
+        self.main_thread.start()
+
+        # start a thread that will check that the first thread is alive
+        thread_waiting = threading.Thread(target=waiting_target)
+        thread_waiting.start()
+
+
+class CallbackPlayer(object):
+    def __init__(self, url=None):
+        self.url = url
+
+    def start(self):
+        PlayerManager.play(self.url, blocking_thread=True)
--- back/webapi/tests.py
+++ back/webapi/tests.py
@@ -1,5 +1,3 @@
-from time import sleep
-
 from django.test import TestCase
 
 # from webapi.CrontabManager import CrontabManager
@@ -25,7 +23,8 @@ from django.test import TestCase
 # test remove
 # test = "piclodio 2"
 # CrontabManager.remove_job(test)
-from webapi.PlayerManager import PlayerManager
+from webapi.PlayerManager import PlayerManager, CallbackPlayer, ThreadTimeout
+
 # from webapi.models import AlarmClock, BackupMusic
 
 # all_jobs = BackupMusic.objects.all()
@@ -37,59 +36,14 @@ from webapi.PlayerManager import PlayerManager
 #
 # PlayerManager.stop()
 
-import threading
 
-
-class ThreadTimeout(object):
-    def __init__(self, callback_instance, backup_instance, timeout):
-        self.callback_instance = callback_instance
-        self.backup_instance = backup_instance
-        self.timeout = timeout
-        self.main_thread = None
-
-    def run(self):
-        def target():
-            print 'Thread started'
-            self.callback_instance.start()
-            print 'Thread finished'
-
-        def waiting_target():
-            print "Wait %s seconds before checking is the thread is alive" % self.timeout
-            sleep(self.timeout)
-            if self.main_thread.is_alive():
-                print 'Thread is alive'
-                # thread.join()
-            else:
-                print 'Thread is dead'
-                if self.backup_instance is not None:
-                    print "Playing backup file"
-                    self.backup_instance.start()
-                else:
-                    print "Not backup file provided"
-
-        # start a thread to play the webradio
-        self.main_thread = threading.Thread(target=target)
-        self.main_thread.start()
-
-        # start a thread that will check that the first thread is alive
-        thread_waiting = threading.Thread(target=waiting_target)
-        thread_waiting.start()
-
-
-
-class CallbackPlayer(object):
-    def __init__(self, url=None):
-        self.url = url
-
-    def start(self):
-        PlayerManager.play(self.url)
 
 
 # working url
 url = "http://192.99.17.12:6410/"
 
 # not working url
-#url = "http://192.99.17.10:6410/"
+url = "http://192.99.17.10:6410/"
 backup_mp3 = "/home/nico/Desktop/mpthreetest.mp3"
 
 url_player = CallbackPlayer(url=url)
