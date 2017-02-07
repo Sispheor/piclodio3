9eafa62f62a4d3f8408ce38a937c45f03f7d94f5
nico <nico.marcq@gmail.com>
2017-02-07 14:22:59 +0100

    fix backup MP3 starting. test is the URL is valid before

--- back/run_web_radio.py
+++ back/run_web_radio.py
@@ -7,10 +7,30 @@ E.g: python run_web_radio.py 12 20
 """
 import os
 import sys
-
+import urllib2
 import django
-
 from webapi.Utils.PlayerManager import CallbackPlayer, ThreadTimeout
+
+
+def is_url_valid(url):
+    """
+    Test if we can get a TCP connection to the target URL
+    :param url: url to check
+    :return: True if the metode can connect to the URL
+    """
+    try:
+        print("Try to connect to the URL: %s" % url)
+        connection = urllib2.urlopen(url, timeout=5)
+        print("URL status code: %s " % connection.getcode())
+        connection.close()
+        return True
+    except urllib2.HTTPError:
+        print("Failed to connect")
+        return False
+    except urllib2.URLError:
+        print("Failed to connect (timeout)")
+        return False
+
 
 # load django models
 project_path = os.path.dirname(os.path.realpath(__file__))
@@ -37,22 +57,32 @@ except WebRadio.DoesNotExist:
     print "The web radio id %s does not exist, cannot launch"
     sys.exit()
 
-# try to get a backup mp3
+# try to get a backup mp3 if exists
 backup_mp3_list = BackupMusic.objects.all()
 backup_mp3_path = None
+backup_mp3_callback = None
 if backup_mp3_list is not None:
     if len(backup_mp3_list) == 1:
         backup_mp3_path = backup_mp3_list[0].backup_file.url
         print "Path to the backup MP3: %s" % backup_mp3_path
+        if backup_mp3_path is not None:
+            backup_mp3_callback = CallbackPlayer(url=backup_mp3_path)
 
-# start the thread that will play the web radio, check that the web radio is playing, and auto kill it if needed
-web_radio_callback = CallbackPlayer(url=web_radio_to_play.url)
-backup_mp3_callback = None
-if backup_mp3_path is not None:
-    backup_mp3_callback = CallbackPlayer(url=backup_mp3_path)
+# test the URL, if this one is not valid, we start the backup
+if is_url_valid(url=web_radio_to_play.url):
+    # start the thread that will play the web radio, check that the web radio is playing, and auto kill it if needed
+    web_radio_callback = CallbackPlayer(url=web_radio_to_play.url)
+    # the following thread will start to play the web radio and then check if the player is still alive after 35 seconds
+    # this to prevent the case where the URL is valid and is answering request but no stream is present inside
+    command = ThreadTimeout(callback_instance=web_radio_callback,
+                            backup_instance=backup_mp3_callback,
+                            timeout=35,
+                            time_before_auto_kill=minute_before_auto_kill)
+    command.run()
+else:
+    # the URL is not valid, start the backup MP3 if exist
+    if backup_mp3_callback is not None:
+        backup_mp3_callback.start()
 
-command = ThreadTimeout(callback_instance=web_radio_callback,
-                        backup_instance=backup_mp3_callback,
-                        timeout=5,
-                        time_before_auto_kill=minute_before_auto_kill)
-command.run()
+
+
